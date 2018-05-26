119fa5cad9414ead748811f5eb37ed996b8417d3
Jalal Maqdisi <jalmaq01@mail.bbk.ac.uk>
2018-05-26 18:47:30 +0100

    Update closure.kt

--- closure.kt
+++ closure.kt
@@ -2,17 +2,18 @@
 fun makeAdder(x: Int) = { y: Int -> x + y }
 
 // using annonymous function
-fun makeMult(x: Int): (Int)->Int { 
-    return fun(y: Int): Int { return x * y }
+fun makeMult(x: Int): (Int) -> Int { 
+	return fun(y: Int): Int { 
+		return x * y 
+	}
 }
 
 
 fun main(args: Array<String>) {
-    println("Hello, world!")
-    val add5 = makeAdder(5)
-    val add10 = makeAdder(10)
+	val add5 = makeAdder(5)
+	val add10 = makeAdder(10)
 	println("add5(10) : ${add5(10)} \nadd10(10) : ${add10(10)}")
     
-    val mult5 = makeMult(5)
-    println("mult5(10) : ${mult5(10)}")
+	val mult5 = makeMult(5)
+	println("mult5(10) : ${mult5(10)}")
 }
